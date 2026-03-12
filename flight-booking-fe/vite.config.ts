13688cce8b4589db7ff8c1b395dc43503335e27b
nkson05 <nkhanhson05@gmail.com>
2026-03-12 22:27:11 +0700

    test

--- flight-booking-fe/vite.config.ts
+++ flight-booking-fe/vite.config.ts
@@ -1,27 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
 import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        configure: (proxy, _options) => {
-          proxy.on('proxyReq', (proxyReq, _req, _res) => {
-            // Strip the Origin header to trick Spring Boot's CorsFilter
-            proxyReq.removeHeader('Origin');
-          });
-        }
-      }
-    }
-  }
+  // plugins: [react(), tailwindcss()],
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8080',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       configure: (proxy, _options) => {
+  //         proxy.on('proxyReq', (proxyReq, _req, _res) => {
+  //           // Strip the Origin header to trick Spring Boot's CorsFilter
+  //           proxyReq.removeHeader('Origin');
+  //         });
+  //       }
+  //     }
+  //   }
+  // }
   plugins: [react()],
   resolve: {
     alias: {
